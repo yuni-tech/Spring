0facba8b3716cec451d7aca9e20af89fe009b9e7
mingo <zzmingo@qq.com>
2019-01-23 15:18:28 +0800

    Update AutoTextView.swift

--- Spring/AutoTextView.swift
+++ Spring/AutoTextView.swift
@@ -14,9 +14,6 @@ public class AutoTextView: UITextView {
         get {
             var size = self.sizeThatFits(CGSize(width: self.frame.size.width, height: CGFloat.greatestFiniteMagnitude))
             size.width = self.frame.size.width
-            if text.length == 0 {
-                size.height = 0
-            }
             
             contentInset = UIEdgeInsets(top: -4, left: -4, bottom: -4, right: -4)
             layoutIfNeeded()
